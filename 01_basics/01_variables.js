61486d05da6d854bf576909114623be4ed1c706e
adityawalke21 <108576254+adityawalke21@users.noreply.github.com>
2024-02-01 00:26:22 +0000

    variables video done

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,12 @@
+const accountId = 334455;
+let accountEmail = "adi@gmail.com";
+var accPassword = "123456";
+accCity = "Nagpur";
+
+// accountId = 112233; // not allowed
+
+accountEmail = "aditya@gmail.com";
+accPassword = "654321";
+accCity = "Wardha";
+
+console.table([accountEmail,accPassword,accCity]);
